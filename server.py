f47869ba919534658bce61b8eba81c1ee43bbe1e
sta <stadoesyt@gmail.com>
2026-03-29 12:48:55 +0100

    Initialize server with logging and protocols

---
+++ server.py
@@ -0,0 +1,15 @@
+# Import the server
+from cloudlink import server
+# Import protocols
+from cloudlink.server.protocols import clpv4, scratch
+# Instantiate the server object
+server = server()
+# Set logging level
+server.logging.basicConfig(
+ level=server.logging.INFO # See python's logging library for details on logging levels.
+)
+# Load protocols
+clpv4 = clpv4(server)
+scratch = scratch(server)
+# Start the server!
+server.run(ip="0.0.0.0", port=3000)
